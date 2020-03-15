c10372743c729803a38e13e6b124c54bf37b914b
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-03-15 14:13:12 -0400

    updating read me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# covid-19
+# Coronavirus Global Data
+
+Visualization for live information on the spread of COVID-19.
+
+![alt text][img/covid19-search]
+
+This is the API I used (https://github.com/javieraviles/covidAPI)
--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,72 @@
-$(function(){
-  new Vue({
-      el: '#app',
-      created() {
-          this.fetchData();
-      },
-      data: {
-           country: [],
-           confirmed:[]
-      },
-      methods: {
-          fetchData() {
-          axios.get('https://coronavirus-tracker-api.herokuapp.com/all').then(response => {
-            let countries = {};
-            const locations = response.data.confirmed.locations;
+const totals = new Vue({
+  el: "#totals",
+  created() {
+    this.fetchData();
+  },
+  data: {
+    totalCases: "",
+    totalDeaths: "",
+    totalRecovered: "",
+  },
+  methods: {
+    fetchData() {
+      axios.get('https://coronavirus-19-api.herokuapp.com/all').then(response => {
+        this.totalCases = new Intl.NumberFormat().format(response.data.cases)
+        this.totalDeaths = new Intl.NumberFormat().format(response.data.deaths)
+        this.totalRecovered = new Intl.NumberFormat().format(response.data.recovered)
+      })
+    }
+  },
+})
 
-            var unique = locations.filter((v, i, a) => a.indexOf(v) === i);
-            console.log(unique)
-
-              this.confirmed = response.data.confirmed.latest;
-              this.country = response.data.confirmed.locations;
-              // console.log(countries)
-            });
+const app = new Vue({
+  el: '#app',
+  created() {
+    this.fetchData();
+  },
+  data: {
+    covid19DataList: [],
+    country: '',
+    searchQuery: null,
+  },
+  methods: {
+    fetchData() {
+      axios.get('https://coronavirus-19-api.herokuapp.com/countries').then(response => {
+        const data = response.data;
+        for (var d in data) {
+          if (!data[d].showDetail) {
+            data[d].showDetail = false;
           }
+        }
+        data.sort(function(a, b) {
+          if (a.country < b.country) {
+            return -1;
+          }
+          if (a.country > b.country) {
+            return 1;
+          }
+          return 0;
+        })
 
+        this.covid19DataList = data;
+      })
+    },
+    toggleDeatils: function(data) {
+      data.showDetail = !data.showDetail
+    },
+    filterList: function() {
+      this.country = event.target.value
+    }
+  },
+  computed: {
+    resultQuery() {
+      if (this.searchQuery) {
+        return this.covid19DataList.filter((item) => {
+          return this.searchQuery.toLowerCase().split(' ').every(v => item.country.toLowerCase().includes(v))
+        })
+      } else {
+        return this.covid19DataList;
       }
-  });
-
-
+    }
+  }
 })
